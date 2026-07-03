9ffc0828070435f4b0ecea74a52c65c94e78e1ce
SARTHAK JAKHMOLA <sarthak.122482@stu.upes.ac.in>
2026-07-03 09:51:19 +0530

    Time: 260 ms (63.63%) | Memory: 348 MB (60.08%) - LeetSync

---
+++ 3919-network-recovery-pathways/network-recovery-pathways.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+        int n = online.size();
+        vector<vector<pair<int, int>>> g(n);
+
+        int l = INT_MAX, r = 0;
+
+        // Build graph using only online nodes
+        for (auto &e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            if (!online[u] || !online[v]) continue;
+            g[u].push_back({v, w});
+            l = min(l, w);
+            r = max(r, w);
+        }
+
+        // No usable edges
+        if (l == INT_MAX) return -1;
+
+        auto check = [&](int mid) -> bool {
+            vector<long long> dist(n, LLONG_MAX / 4);
+            dist[0] = 0;
+
+            priority_queue<
+                pair<long long, int>,
+                vector<pair<long long, int>>,
+                greater<pair<long long, int>>
+            > pq;
+
+            pq.push({0, 0});
+
+            while (!pq.empty()) {
+                auto [d, u] = pq.top();
+                pq.pop();
+
+                if (d > k) return false;
+                if (u == n - 1) return true;
+                if (d != dist[u]) continue;
+
+                for (auto &[v, w] : g[u]) {
+                    if (w < mid) continue;
+
+                    long long nd = d + w;
+                    if (nd < dist[v]) {
+                        dist[v] = nd;
+                        pq.push({nd, v});
+                    }
+                }
+            }
+
+            return false;
+        };
+
+        while (l < r) {
+            int mid = l + (r - l + 1) / 2;
+            if (check(mid))
+                l = mid;
+            else
+                r = mid - 1;
+        }
+
+        return check(l) ? l : -1;
+    }
+};
