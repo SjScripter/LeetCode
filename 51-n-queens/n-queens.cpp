91b50d88b100fda6466a51640b3b824e62378bc4
SARTHAK JAKHMOLA <sarthak.122482@stu.upes.ac.in>
2026-07-07 10:06:01 +0530

    Time: 3 ms (62.12%) | Memory: 11 MB (28.14%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<int> colOccupied(n);
+        vector<int> diagonalOccupied(n << 1);
+        vector<int> antiDiagonalOccupied(n << 1);
+        vector<vector<string>> result;
+        vector<string> currentBoard(n, string(n, '.'));
+        function<void(int)> backtrack = [&](int row) -> void {
+            if (row == n) {
+                result.push_back(currentBoard);
+                return;
+            }
+          
+            for (int col = 0; col < n; ++col) {
+                if (colOccupied[col] + diagonalOccupied[row + col] + 
+                    antiDiagonalOccupied[n - row + col] == 0) {
+                  
+                    currentBoard[row][col] = 'Q';
+                  
+                    colOccupied[col] = 1;
+                    diagonalOccupied[row + col] = 1;
+                    antiDiagonalOccupied[n - row + col] = 1;
+                  
+                    backtrack(row + 1);
+
+                    colOccupied[col] = 0;
+                    diagonalOccupied[row + col] = 0;
+                    antiDiagonalOccupied[n - row + col] = 0;
+                    currentBoard[row][col] = '.';
+                }
+            }
+        };
+        backtrack(0);
+      
+        return result;
+    }
+};
