928b9bb06a4b2710823a911fa771685dbbb7abcc
SARTHAK JAKHMOLA <sarthak.122482@stu.upes.ac.in>
2026-07-26 18:40:41 +0530

    Time: 16 ms (18.84%) | Memory: 31.6 MB (46.18%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -3,7 +3,6 @@ class Solution {
   int maximumProduct(vector<int>& nums) {
     const int n = nums.size();
     ranges::sort(nums);
-    return max(nums[n - 1] * nums[0] * nums[1],
-               nums[n - 1] * nums[n - 2] * nums[n - 3]);
+    return max(nums[n - 1] * nums[0] * nums[1],nums[n - 1] * nums[n - 2] * nums[n - 3]);
   }
 };
