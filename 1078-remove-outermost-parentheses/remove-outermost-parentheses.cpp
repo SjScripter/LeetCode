77855255894fae23013e8239aa52592a0f11591b
SARTHAK JAKHMOLA <sarthak.122482@stu.upes.ac.in>
2026-07-31 20:56:57 +0530

    Time: 28 ms (5.32%) | Memory: 67.3 MB (5.45%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string result;
-        int c=0;
+        int count=0;
+        string ans = "";
 
-        for(char ch: s){
-            if(ch== '('){
-                if(c>0){
-                result = result +ch;
+        for(char cc : s){
+            if(cc == '('){
+                if(count>0){
+                ans = ans + cc;
                 }
+                count ++;
+                
+            }
+            if(cc == ')'){
+                count --;
+                if(count > 0){
+                    ans = ans+cc;
+                }
+            }
 
-                c++;
-            }
-            else if(ch ==')'){
-                c--;
-                if(c>0){
-                    result += ch;
         }
-            }
-        }
-        return result;
+        return ans;
     }
 };
