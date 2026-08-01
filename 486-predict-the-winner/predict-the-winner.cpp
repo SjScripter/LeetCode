67cf84588045cb3338624a756fb4d4bfd3c77622
SARTHAK JAKHMOLA <sarthak.122482@stu.upes.ac.in>
2026-08-01 09:50:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (85.12%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  bool predictTheWinner(vector<int>& nums) {
+    const int n = nums.size();
+    vector<int> dp = nums;
+
+    for (int d = 1; d < n; ++d)
+      for (int j = n - 1; j - d >= 0; --j) {
+        const int i = j - d;
+        dp[j] = max(nums[i] - dp[j],nums[j] - dp[j - 1]);  
+      }
+
+    return dp[n - 1] >= 0;
+  }
+};
