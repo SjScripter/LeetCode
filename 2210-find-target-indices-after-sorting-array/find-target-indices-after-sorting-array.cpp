6fea8baae25cec5a2e3b28d7031223721fe33909
SARTHAK JAKHMOLA <sarthak.122482@stu.upes.ac.in>
2026-08-08 14:59:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (85.90%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -3,22 +3,15 @@ public:
     vector<int> targetIndices(vector<int>& nums, int target) {
 
         vector<int> ex;
-        vector<int> tt;
-        tt = nums;
         sort(nums.begin(),nums.end());
-
         for(int i = 0 ;i<nums.size();i++){
             if(nums[i] == target){
                 ex.push_back(i);
             }
 
-        }
-        
-        if(ex.empty()){
-            return {};
-        }
-        else{
+        } 
+       
             return ex;
-        }
+        
     }
 };
