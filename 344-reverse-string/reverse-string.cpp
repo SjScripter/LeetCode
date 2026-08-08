07374d04bc7336c1547b556650bbc60efe43c1b6
SARTHAK JAKHMOLA <sarthak.122482@stu.upes.ac.in>
2026-08-08 19:38:21 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (81.75%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    
+    void reverseString(vector<char>& s) {
+
+        int n = s.size();
+        int i = 0;
+
+        while(i<(n/2)){
+            int t = s[i];
+            s[i] = s[n-1-i];
+            s[n-1-i] = t;
+            i++;
+        }
+
+    }
+};
