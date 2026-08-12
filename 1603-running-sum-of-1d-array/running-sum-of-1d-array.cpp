8cc28bf9201a537c8597df80a0c58d4f83d176a3
SARTHAK JAKHMOLA <sarthak.122482@stu.upes.ac.in>
2026-08-13 01:51:48 +0530

    Time: 3 ms (5.40%) | Memory: 12.5 MB (53.52%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sum = 0;
+        int n =nums.size();
+        vector<int> arr(n);
+
+        for(int i = 0;i<n;i++){
+
+            sum = sum +nums[i];
+            arr[i] = sum;
+
+        }
+
+        return arr;
+        
+    }
+};
