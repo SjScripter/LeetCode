d3103574db5fa51d7596ab058376d5b9e6706706
SARTHAK JAKHMOLA <sarthak.122482@stu.upes.ac.in>
2026-09-12 14:50:56 +0530

    Time: 147 ms (5.03%) | Memory: 13.9 MB (98.86%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
 
         for(int i =0;i<nums.size();i++){
-            for(int j = i+1;j<nums.size();j++){
-
+            for(int j = 0;j<nums.size();j++){
+                if(i == j) continue;
                 if(nums[i] + nums[j] == target){
                     return {i,j};
                 }
-
             }
         }
-        return {};
+        return {-1,-1};
+        
     }
 };
